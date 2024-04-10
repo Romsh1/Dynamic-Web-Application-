190f0821808fbc5e3702c7f9152b5ca5718e1319
Romika <romika2002chau.21@gmail.com>
2024-04-10 19:08:39 -0400

    Update on locale time

--- Dynamic-Web-Application-/script.js
+++ Dynamic-Web-Application-/script.js
@@ -17,6 +17,26 @@ const myBtn = document.querySelector('.myForm');
 let curr_category = "hill";
 
 //Function to fetch and display images
+// function getPhotos(picSearch) {
+//     fetch(`${URL}${picSearch}&&count=99&client_id=${API_KEY}`) 
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data);
+//         if (data.results.length > 0) {
+//             let randNumber = Math.floor(Math.random() * data.results.length);
+//             const imageUrl = data.results[randNumber].urls.regular;
+//             //Setting the src attribute of the img element
+//             placeImage.src = imageUrl; 
+//         } else {
+//             console.error('No images found for the search item.');
+//         }
+//     })
+//     .catch(error => {
+//         console.error('Error fetching data:', error);
+//     });
+// }
+
+//Function to fetch and display images using AJAX
 function getPhotos(picSearch) {
     $.ajax({
         url: `${URL}${picSearch}&&count=99&client_id=${API_KEY}`,
@@ -39,32 +59,34 @@ function getPhotos(picSearch) {
     });
 }
 
-
-$(document).ready(function() {
-    $('#myBtn').on('submit', function(e) {
-        e.preventDefault();
-        let picSearch = $('#searchInput').val().trim();
-        if (picSearch === "") {
-            alert("Please enter a search term.");
-            return;
-        } else {
-            curr_category = picSearch;
-            getPhotos(picSearch);
-            //Clearing the search input after submitting the form
-            $('#searchInput').val(""); 
-        }
-    });
-
-    // Initial call to fetch and display images
-    // getPhotos();
-
-    // Refreshing images every 20 seconds
-    setInterval(function() {
-        getPhotos(curr_category);
-    }, 20000); 
+//Event listener for form submission
+myBtn.addEventListener('submit', e => {
+    e.preventDefault();
+    //Using the search input value and trim whitespace
+    let picSearch = searchInput.value.trim(); 
+    if (picSearch === "") {
+        //Displaying an alert if the search box is empty
+        alert("Please enter a search term."); 
+        //Exitting the function early if the search term is empty
+        return; 
+    } else {
+        curr_category = picSearch;
+        getPhotos(picSearch);
+        //Clearing the search input after submitting the form
+        searchInput.value = "";     
+    }
 });
 
+//Initial call to fetch and display images
+// getPhotos();
 
+//Refreshing images every 20 seconds
+setInterval(() => {
+    getPhotos(curr_category);
+}, 2000); 
+
+
+// Weather API
 //Declaring current city and units
 let curr_city = "Sarnia";
 let units = "metric";
@@ -82,121 +104,125 @@ let weatherWind = document.querySelector(".weather_wind");
 let weatherPressure = document.querySelector(".weather_gauge");
 let loadStatus = document.querySelector(".loading");
 
-$(document).ready(function() {
-    //Search functionality
-    $(".search-weather").on('submit', function(e) {
-        let search = $(".weather_searchForm").val();
-        //Preventing default action
-        e.preventDefault();
-        //Changes current city according to the value entered
-        curr_city = search;
-        //Gets the weather forecast
+//Search functionality
+document.querySelector(".search-weather").addEventListener
+('submit', e => {
+    let search = document.querySelector(".weather_searchForm");
+    //Preventing default action
+    e.preventDefault();
+    //Changes current city according to the value entered
+    curr_city = search.value;
+    //Gets the weather forecast
+    getWeather();
+    //Clearing input text box after each search
+    search.value = "";
+})
+
+//Converting Celcius to Fahrenheit and Vice-Versa
+document.querySelector(".weather_temp_celcius").addEventListener
+('click', () => {
+    if(units !== "metric"){
+        //Changing to metric
+        units = "metric"
+        //Getting the weather forecast
         getWeather();
-        //Clearing input text box after each search
-        $(".weather_searchForm").val("");
-    });
-
-    //Converting Celcius to Fahrenheit and Vice-Versa
-    $(".weather_temp_celcius").on('click', function() {
-        if (units !== "metric") {
-            //Changing to metric
-            units = "metric";
-            //Getting the weather forecast
-            getWeather();
-        }
-    });
-
-    $(".weather_temp_fahrenheit").on('click', function() {
-        if (units !== "imperial") {
-            units = "imperial";
-            getWeather();
-        }
-    });
-
-    function convertTimeStamp(timestamp, timezone) {
-        //Converting seconds to hour
-        const convertTimezone = timezone / 3600;
-
-        const date = new Date(timestamp * 1000);
-
-        const options = {
-            weekday: "long",
-            day: "numeric",
-            month: "long",
-            year: "numeric",
-            hour: "numeric",
-            minute: "numeric",
-            //Setting the timezone for date formatting
-            timeZone: `Etc/GMT${convertTimezone >= 0 ? "-" : "+"}${Math.abs(convertTimezone)}`,
-            //Specifying to use 12-hour time format
-            hour12: true,
-        };
-        return date.toLocaleString("en-US", options);
     }
+})
 
-    //Converting country code to Country name
-    function convertCountryCode(country) {
-        let regionNames = new Intl.DisplayNames(["en"], { type: "region" });
-        //Returning the displayed name of provided country codes
-        return regionNames.of(country);
+document.querySelector(".weather_temp_fahrenheit").addEventListener
+('click', () => {
+    if(units !== "imperial"){
+        units = "imperial"
+        getWeather();
     }
+})
 
-    //Function to update date and time every second
-    function updateDateTime() {
-        const datetimeDiv = $('.weather_dateNtime');
-        setInterval(function() {
-            const now = new Date();
-            datetimeDiv.html(now.toLocaleString());
-        }, 1000);
-    }
+function convertTimeStamp(timestamp, timezone) {
+    //Converting seconds to hour
+    const convertTimezone = timezone / 3600;
 
-    //Calling the updateDateTime function when the page loads
-    $(updateDateTime);
-});
+    const date = new Date(timestamp * 1000);
 
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        //Setting the timezone for date formatting
+        timeZone: `Etc/GMT${convertTimezone >= 0 ? "-" : "+"}${Math.abs(convertTimezone)}`,
+        //Specifying to use 12 hour time format
+        hour12: true,
+    };
+    return date.toLocaleString("en-US", options);
+}
 
+//Converting country code to Country name
+function convertCountryCode(country) {
+    let regionNames = new Intl.DisplayNames(["en"], { type: "region" });
+    //Returns the display name of provided country codes
+    return regionNames.of(country);
+}
+
+//Function to update date and time every second
+function updateDateTime() {
+    const datetimeDiv = document.querySelector('.weather_dateNtime');
+    setInterval(() => {
+        const now = new Date();
+        datetimeDiv.innerHTML = now.toLocaleString();
+    }, 1000);
+}
+
+// Call the updateDateTime function when the page loads
+document.addEventListener('DOMContentLoaded', updateDateTime);
+
+//Main function with API call
 function getWeather() {
     const API_KEY = 'b0bd8bbd5f515361fdc416b758befdc5';
     //Validation if user input for city is not entered
-    if (!curr_city) {
+    if(!curr_city){
         alert("Please enter a city!");
         return;
     }
 
     //Showing loading message
-    $('.loadStatus').css('display', 'block');
+    loadStatus.style.display = 'block';
 
-    $.ajax({
-        url: `https://api.openweathermap.org/data/2.5/weather?q=${curr_city}&appid=${API_KEY}&units=${units}`,
-        method: 'GET',
-        success: function(data) {
-            console.log(data);
-            //Processing the received data
-            $('.city').html(`${data.name}, ${convertCountryCode(data.sys.country)}`);
-            $('.dateTime').html(convertTimeStamp(data.dt, data.timezone));
-            $('.weatherForecast').html(`<p>${data.weather[0].main}</p>`);
-            $('.weatherTemperature').html(`${data.main.temp.toFixed()}${units === "imperial" ? "&#176F" : "&#176C"}`);
-            $('.weatherIcon').html(`<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="Weather Icon"/>`);
-            $('.weatherMinMax').html(`<p>H: ${data.main.temp_min.toFixed()}&#176 </p>
-                                    <p>L: ${data.main.temp_max.toFixed()}&#176 </p>`);
-            $('.weatherFeelsLike').html(`${data.main.feels_like.toFixed()}${units === "imperial" ? "&#176F" : "&#176C"}`);
-            $('.weatherHumidity').html(`${data.main.humidity}%`);
-            $('.weatherWind').html(`${data.wind.speed}${units === "imperial" ? "mph" : "m/s"}`);
-            $('.weatherPressure').html(`${data.main.pressure}hpa`);
-
-            //Hiding loading message after data is fetched
-            $('.loadStatus').css('display', 'none');
-        },
-        error: function(xhr, status, error) {
-            console.error('Error fetching weather data:', error);
-            if (error.message === 'City not found') {
-                alert("Invalid city name!");
-            }
-            //Hiding loading message in case of error
-            $('.loadStatus').css('display', 'none');
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${curr_city}&appid=${API_KEY}&units=${units}`)
+    .then(res => {
+        if (!res.ok) {
+            throw new Error('City not found');
         }
+        return res.json();
+    })
+    .then(data => {
+        console.log(data);
+        //Processing the received data
+        city.innerHTML = `${data.name}, ${convertCountryCode(data.sys.country)}`;
+        dateTime.innerHTML = convertTimeStamp(data.dt, data.timezone);
+        weatherForecast.innerHTML = `<p>${data.weather[0].main}</p>`;
+        weatherTemperature.innerHTML = `${data.main.temp.toFixed()}${units === "imperial" ? "&#176F" : "&#176C"}`;
+        weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="Weather Icon"/>`;
+        weatherMinMax.innerHTML = `<p>H: ${data.main.temp_min.toFixed()}&#176 </p>
+                                    <p>L: ${data.main.temp_max.toFixed()}&#176 </p>`;
+        weatherFeelsLike.innerHTML = `${data.main.feels_like.toFixed()}${units === "imperial" ? "&#176F" : "&#176C"}`;
+        weatherHumidity.innerHTML = `${data.main.humidity}%`;
+        weatherWind.innerHTML = `${data.wind.speed}${units === "imperial" ? "mph" : "m/s"}`;
+        weatherPressure.innerHTML = `${data.main.pressure}hpa`;
+
+        //Hiding loading message after data is fetched
+        loadStatus.style.display = 'none';
+    })
+    .catch(error => {
+        console.error('Error fetching weather data:', error);
+        if (error.message === 'City not found') {
+            alert("Invalid city name!");
+        }
+        //Hiding loading message in case of error
+        loadStatus.style.display = 'none';
     });
 }
 
 //After the doc will be fully loaded and parsed, getWeather() function will be called
-$(getWeather);
+document.addEventListener("DOMContentLoaded", getWeather);
